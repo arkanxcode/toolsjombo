0eddd10abede16c58c6a5a66f4b9dada9a492aed
arkanxcode <57281915+arkanxcode@users.noreply.github.com>
2019-11-03 11:20:36 +0700

    Add files via upload

---
+++ toolsjomblo.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+echo "\033[1;96m█████████"
+echo "\033[1;96m█▄█████▄█"
+echo "\033[1;96m█ ▼▼▼▼▼  _-_-- ●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●"
+echo "\033[1;96m█.    _-_-- -_ --  Ark.N_/Sn.Xcod3"
+echo "\033[1;96m█ ▲▲▲▲▲   -_ -«============✧==========»"
+echo "\033[1;96m█████████   SUBSCRIBE NOW TO MY CHANNELL ...!!"
+echo "\033[1;96m ██ ██       VIP-1.9 Channel Arkan Channel"
+echo "\033[1;93m╔═══════════════════════════════════════════╗"
+echo "\033[1;93m║\033[1;96m¤ \033[1;93mAuthor   \033[1;93m: \033[1;93mMr_Ark.N\033[1;93m                      ║"
+echo "\033[1;93m║\033[1;96m¤ \033[1;93mWa  \033[1;93m    : \033[1;93m\033[4m089501720842\033[0m \033[1;93m                  ║"
+echo "\033[1;93m║\033[1;96m¤ \033[1;93mEmail  \033[1;93m : \033[1;93m\033[4mresultfb707@gmail.com  \033[0m \033[1;93m       ║"
+echo "\033[1;93m╚═══════════════════════════════════════════╝"
+
+sleep 3
+
+ulang="y"
+while [ $ulang = "y" ]
+do
+echo "\033[1;94m●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●"
+echo "Silahkan Pilih Tools Baperan Anda !"
+echo "(1) Kumpulan Gombalan Ter GG"
+echo "(2) Nembak Ceue Via Termux"
+echo "(3) Nykung Ceue Orang INSTANT !"
+echo "(4) Exit"
+echo "«====================✧====================»"
+echo "\033[37;1m"
+read -p "Masukan Nomor Yang Anda Pilih ! :" pilih;
+if [ $pilih ="1" ]
+then
+	clear
+	echo "\033[0;36mAku sudah siap kalau senin harus bangun pagi, apalagi bangun rumah tangga sama kamu"
+	sleep 2
+	echo "\033[0;36mCintaku ke kamu tuh kayak utang, awalnya kecil, didiemin, tau-tau gede sendiri"
+	sleep 2
+	echo "\033[0;36mKamu punya spidol item ga? Buat apa? Mau warnain kalender, biar ga ada kata libur dalam mencintaimu"
+	sleep 2
+	echo "\033[0;36mOrangtuamu pengrajin bantal yah? Karena terasa nyaman jika di dekatmu"
+	sleep 2
+	echo "\033[0;36mKopi ini pahit tapi kalau minum sambil melihat dirimu, kopi ini terasa manis deh"
+	sleep 2
+	echo "\033[0;36mManisnya martabak kacang-coklat ini tak semanis wajah indahmu"
+	echo "\033[37;1m"
+	sleep 2
+elif [ $pilih ="2" ]
+then
+	clear
+	sleep 2
+	echo "\033[1;92mMaukah ente menjadi istriku ?"
+	echo "1.Mau !"
+	echo "2.Ngak Mau "
+	echo "\033[37;1m"
+	sleep 2
+	read -p "Masukan Nomor Yang Anda Pilih! :" pilih;
+	if [ $pilih ="1" ]
+	then
+		clear
+		sleep 2
+		echo "Aku berjanji akan cinta sampai mati sama kamu"
+	elif [ $pilih ="2" ]
+		clear
+		sleep 2
+		echo "Yahhh....Jomblo Lagi Deh..."
+		sleep 2
+elif [ $pilih ="3" ]
+then
+	clear
+	sleep 2 
+	echo "Isi Data Diri Ceue Yang Mau anda TYKUNG"
+	read -p "Masukan Nama Lengkap Ceue Targer Anda :" nama
+	echo "Telah Berhasil Di TYKUNG...Silahkan Tunggu 24Jam !" $nama
+	sleep 2
+	clear
+elif [ $pilih ="4" ]
+then
+	clear
+	sleep 2
+	echo "Exit"
+	echo "Terima Kasih Sudah Menggunakan Tools Saya.....Salam Arkan Channel"
+	sleep 2
+	Exit
+else
+	echo "Input Yang Anda Masukan Salah !! Ulangi !!...."
+	sleep 2
+	echo $ulang
+fi
+done
+
+
+
+
+		
+
+
+
+
+
